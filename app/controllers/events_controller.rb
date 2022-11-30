8ce7bcedb4213f8f125c951ca18409f56e5e7850
dylan <dyan2009200@gmail.com>
2022-11-30 15:30:02 +0800

    新增devise以及boostrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,15 @@
 class EventsController < ApplicationController
+  before_action :events, only: [:show]
+  
   def index
+    @events = Event.all
+  end
+  
+  def show
+  end
+
+  private
+  def find_event
+    @events = Event.find(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events
-  resources :employees
-  resources :clients
+  # resources :employee
+  # resources :clients
+
+  namespace :admin do
+    root "events#index"
+    resources :events
+  end
+
+  root "events#index"
 
 end
